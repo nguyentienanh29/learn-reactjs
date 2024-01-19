335a5bbd1d1159b6da99104f31de18ba20998686
Tien Anh <anhnguyentien3006@gmail.com>
2024-01-19 17:53:24 +0700

    useEffect

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -22,14 +22,53 @@ function TodoFeature(props) {
     },
   ];
 
+  //STATE
   const [todoList, setTodoList] = useState(initTodoList);
+  const [filterStatus, setFilterStatus] = useState("all");
 
-  const handleTodoClick = (todo, idx) => {};
+  const handleTodoClick = (todo, idx) => {
+    //clone 1 mãng mới từ mãng củ
+    const newTodoList = [...todoList]; // dùng cú pháp toán tử spread để clone mãng củ qua cho mảng mới
+
+    //togole state
+    newTodoList[idx] = {
+      ...newTodoList[idx], // đang gán giá trị của objet này cho biến
+      status: newTodoList[idx].status === "new" ? "completed" : "new",
+    };
+
+    //update list
+    setTodoList(newTodoList);
+  };
+
+  //show All truyền vào 1 hàm xử lý
+  const handleShowAllClick = () => {
+    setFilterStatus("all");
+  };
+
+  //show Completed
+  const handleShowCompletedClick = () => {
+    setFilterStatus("completed");
+  };
+
+  //show New
+  const handleShowNewlClick = () => {
+    setFilterStatus("new");
+  };
+
+  const renderedTodoList = todoList.filter(
+    (todo) => filterStatus === "all" || filterStatus === todo.status
+  );
 
   return (
     <div>
       <h3>Todo List</h3>
-      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
+      <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+
+      <div>
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowNewlClick}>Show New</button>
+      </div>
     </div>
   );
 }
