b0c23a6c0b0035b6846b47a52955f98d29d1c982
Tien Anh <anhnguyentien3006@gmail.com>
2024-02-01 17:33:47 +0700

    đang làm Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-// import './App.css';
+//import './App.css';
+import React from 'react';
 import TodoFeature from './features/Todo';
 import SongFeature from './features/Song';
 import Counter from './features/Counter'
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
-      <TodoFeature />
+      HomePage
+      <Routes>
+        <Route path="/todos" element={<TodoFeature />} />
+        <Route path="/albums" element={<SongFeature />} />
 
+      </Routes>
     </div>
   );
 }
