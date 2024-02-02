282fb687f8b4a1075453af5f97fbcc886f763b7d
Tien Anh <anhnguyentien3006@gmail.com>
2024-02-02 16:57:14 +0700

    commit navlink link

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import React from 'react';
 import TodoFeature from './features/Todo';
 import SongFeature from './features/Song';
 import Counter from './features/Counter'
-import { Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
+import { Route, Routes, Link, NavLink } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div className="App">
       HomePage
+      <p><Link to="/todos">Todos</Link></p>
+      <p><Link to="/albums">Albums</Link></p>
+
+      <p><NavLink to="/todos">Todos</NavLink></p>
+      <p><NavLink to="/albums">Albums</NavLink></p>
+
       <Routes>
+
+
         <Route path="/todos" element={<TodoFeature />} />
         <Route path="/albums" element={<SongFeature />} />
-
       </Routes>
     </div>
   );
